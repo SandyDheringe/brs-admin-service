d383707067d9c0d95e21d44215dadeba0daecca8
sandeep <sandeep.ariseup@gmail.com>
2023-08-17 17:11:55 +0530

    first commit

---
+++ src/main/java/com/brsadminservice/BrsAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.brsadminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrsAdminServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrsAdminServiceApplication.class, args);
+    }
+
+}
