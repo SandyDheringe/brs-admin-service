5995b89ec003697684e33f54ce19fc8cbe180a2e
sandeep <sandeep.ariseup@gmail.com>
2023-08-31 01:12:08 +0530

    docker changes

--- src/main/java/com/brsadminservice/BrsAdminServiceApplication.java
+++ src/main/java/com/brsadminservice/BrsAdminServiceApplication.java
@@ -2,12 +2,18 @@ package com.brsadminservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BrsAdminServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BrsAdminServiceApplication.class, args);
+//        SpringApplication.run(BrsAdminServiceApplication.class, args);
+
+        new SpringApplicationBuilder()
+                .profiles("dev")
+                .sources(BrsAdminServiceApplication.class)
+                .run(args);
     }
 
 }
